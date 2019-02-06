1e948b282e8cde0f88a6431df016d37c0011b3a3
robin3317 <abdurrahmanrobin47@gmail.com>
2019-02-07 03:04:40 +0600

    Complete Header section

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,15 @@
-import React, { Component } from 'react';
+// Packages
+import React, { Component, Fragment } from 'react';
+
+// Components
+import Header from './Header';
 
 class App extends Component {
   render() {
     return(
-      <div>App</div>
+      <Fragment>
+        <Header title='BlogSite' />
+      </Fragment>
     );
   }
 }
---
+++ src/components/Header.js
@@ -0,0 +1,32 @@
+// Packages
+import React, { Fragment } from 'react';
+import PropTypes from 'prop-types';
+
+// Material components
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography';
+
+const Header = props => {
+  return(
+    <Fragment>
+      <AppBar position='static' color='primary'>
+        <Toolbar>
+          <Typography variant='h5' color='inherit'>
+            {props.title}
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </Fragment>
+  );
+}
+
+Header.defaultProps = {
+  title: 'Brand Name'
+}
+
+Header.propTypes = {
+  title: PropTypes.string
+}
+
+export default Header;
